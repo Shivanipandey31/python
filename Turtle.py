e9287a3000f2e451902a0db04fca213fdc705eda
SHIVANI PANDEY <shivanipande486@gmail.com>
2025-11-09 12:01:22 +0530

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -14,7 +14,7 @@
 from prettytable import PrettyTable
 table=PrettyTable()
 table.field_names=["Name","Age","Country"]
-table.add_row(["John", 30, "USA"])
+table.add_row(["kairish", 30, "USA"])
 table.add_row(["Anna", 25, "UK"])
-table.add_row(["Peter", 35, "Australia"])
+table.add_row(["Narmis", 35, "Australia"])
 print(table)  
